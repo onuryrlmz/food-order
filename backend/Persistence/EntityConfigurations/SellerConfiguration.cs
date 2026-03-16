589508be8467e893a63b41caedc04b52a1060cc5
onuryrlmz <onuryorulmaz9@gmail.com>
2026-03-16 23:29:40 +0300

    fix: resolve Seller type/namespace ambiguity in SellerConfiguration

--- backend/Persistence/EntityConfigurations/SellerConfiguration.cs
+++ backend/Persistence/EntityConfigurations/SellerConfiguration.cs
@@ -1,12 +1,11 @@
-﻿using Domain.Entities.Seller;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Persistence.EntityConfigurations;
 
-public class SellerConfiguration : IEntityTypeConfiguration<Seller>
+public class SellerConfiguration : IEntityTypeConfiguration<Domain.Entities.Seller.Seller>
 {
-    public void Configure(EntityTypeBuilder<Seller> builder)
+    public void Configure(EntityTypeBuilder<Domain.Entities.Seller.Seller> builder)
     {
         builder.ToTable("Seller").HasKey(c => c.Id);
     }
